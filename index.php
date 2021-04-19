59154b157afe0d25a4a2dcf1661801b0a8a96915
Nur Ikhsan Fajri <57824066+ikhsansss@users.noreply.github.com>
2021-04-19 11:52:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="assets/polinela.png">
+</head>
+<body>
+	
+
+<div class="login">
+
+	<div class="icon"><img src="assets/polinela.png" alt=""></div>
+	<div class="judul j-biru" style="background: #fff;">
+		<h3>SISTEM INFORMASI DATA PROGRAM STUDI</h3>
+	</div>
+
+	<div class="form-login">
+		<h3>LOGIN APLIKASI</h3>
+		<label for=""></label>
+		<form action="" method="post" class="full">
+			<table class="full">
+			<label for="">Username</label>
+			<input type="text" name="username" placeholder="Username" required="" class="full">
+			<label for="">Password</label>
+			<input type="password" name="password" placeholder="Password" required="" class="full">
+			<br><br>
+			<label for=""></label>
+			<input type="submit" name="login" value="Login" class="full hijau">
+			
+		</form>
+	</div>
+</div>
+
+
+</body>
+</html>
